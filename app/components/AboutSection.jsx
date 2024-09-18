d5604439d1b02ea66a88e753cb527c81f1de298c
Ravi kushwaha <77044005+raviikushh@users.noreply.github.com>
2024-09-18 19:34:41 +0530

    Update AboutSection.jsx

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -59,7 +59,7 @@ const AboutSection = () => {
         <Image src="/images/about-image.png" height={500} width={500} alt='about-image'/>
         <div className='mt-4 md:mt-0 text-left flex flex-col h-full '>
             <h2 className='text-4xl font-bold text-white mb-4 '>About Me</h2>
-            <p className='text-base lg:text-lg '>I am a final-year B.Tech student in Computer Science with a strong passion for software development. Currently, I am immersed in ReactJS and Next.js, working towards becoming a proficient MERN stack developer. I have completed projects using HTML, CSS, and JavaScript and actively contribute to open source projects, including Hacktoberfest 2023.</p>
+            <p className='text-base lg:text-lg '>I am a recent graduate with a B.Tech in Computer Science and a strong passion for software development. Currently, I am immersed in ReactJS and Next.js, working towards becoming a proficient MERN stack developer. I have completed projects using HTML, CSS, and JavaScript and actively contribute to open source projects, including Hacktoberfest 2023.</p>
             <div className="flex flex-row justify-start mt-8">
                 <TabButton selectTab={()=> handleTabChange("skills")} 
                     active = {tab === "skills"}>
